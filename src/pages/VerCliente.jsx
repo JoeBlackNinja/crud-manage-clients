518df63eb054fd869df43caaab9eb3e73c20e85c
JoeBlackNinja <jose.nava@alumnos.udg.mx>
2022-05-27 20:56:33 -0500

    175 00:00

--- src/pages/VerCliente.jsx
+++ src/pages/VerCliente.jsx
@@ -1,17 +1,68 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const VerCliente = () => {
 
     const {id} = useParams();
-    console.log(id);
+    const [cliente,setCliente] = useState([]);
+  
+    useEffect(() => {
+      const obtenerClienteAPI = async () => {
+        try {
+          const url = `http://localhost:4000/clientes/${id}`;
+          const peticion = await fetch(url);
+          const resultado = await peticion.json();
+          setCliente(resultado);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+      obtenerClienteAPI();
+    },[])
 
-  return (
-    <div>
-        <h1>
-            Ver cliente.jsx
-        </h1>        
-    </div>
-  )
+    return (
+      <div>
+          <p className='font-black text-4xl text-blue-900 mb-10 uppercase'>
+            Información del cliente  
+          </p>        
+          {cliente.nombre && (
+            <p className='text-4xl text-gray-700 mb-6'>
+              <span className='uppercase font-bold'>Nombre: </span>
+              {cliente.nombre}
+            </p>
+          )}  
+
+          {cliente.email && (
+            <p className='text-4xl text-gray-700 mb-6'>
+              <span className='uppercase font-bold'>Email: </span>
+              {cliente.email}
+            </p>
+          )}            
+
+          {cliente.telefono && (
+            <p className='text-4xl text-gray-700 mb-6'>
+              <span className='uppercase font-bold'>Teléfono: </span>
+              {cliente.telefono}
+            </p>
+          )}
+           
+
+          {cliente.empresa && (
+            <p className='text-4xl text-gray-700 mb-6'>
+              <span className='uppercase font-bold'>Empresa: </span>
+              {cliente.empresa}
+            </p>
+          )}          
+
+          {cliente.notas && (
+          <p className='text-4xl text-gray-700 mb-6'>
+             <span className='uppercase font-bold'>Observaciones: </span>
+             {cliente.notas}
+          </p>
+          )}
+
+      </div>
+    )
 }
 
 export default VerCliente;
